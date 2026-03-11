f63ead3f3f65da7c116cd770f6abc44ec436f742
bhaswar161 <bhaswarray@gmail.com>
2026-03-11 19:46:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import { NextAuthProvider } from "./providers";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NextAuthProvider>{children}</NextAuthProvider>
+      </body>
     </html>
-  )
+  );
 }
