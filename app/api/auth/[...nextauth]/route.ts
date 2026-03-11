747d8fd1e21f716b3598bc83966a5be1cd0a4a49
bhaswar161 <bhaswarray@gmail.com>
2026-03-11 18:30:29 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,12 +9,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    // This forces the redirect to use your main production URL
-    async redirect({ baseUrl }) {
-      return "https://studyhub-chi-eight.vercel.app/api/auth/callback/google"
-    },
-  },
 })
 
 export { handler as GET, handler as POST }
